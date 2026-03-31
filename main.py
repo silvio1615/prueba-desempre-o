23a440f448a339888114f910ee7f28c5b7d872df
Silvio Barrios <56333049+silvio1615@users.noreply.github.com>
2026-03-31 11:05:27 -0500

    commit y error

--- main.py
+++ main.py
@@ -11,6 +11,53 @@ while running:
     elif option == "3":
         busqueda = input("Ingrese un id o nombre del estudiante a buscar")
         feature.search_student(busqueda,feature.date_student)
+    elif option == "4":
+      
+
+       feature.actualizar_info(feature.date_student)
+    elif option == "5":
+       
+       files.guardar_csv(feature.date_student,"student.csv")
+    elif option == "6":
+       Name = input("ingrese el nombre del estudiante: ")
+       feature.eliminar_student(Name,feature.date_student)
+    elif option == "7":
+     print("exit system ")
+     break
+    else:
+       print("Invalid option. Please try again.")
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""import feature
+import files
+running=True
+while running:
+    feature.menu()
+    option= input("select an opcion: ")
+    if option == "1":
+        feature.add_student()
+    elif option == "2":
+        feature.student_consult()
+    elif option == "3":
+        busqueda = input("Ingrese un id o nombre del estudiante a buscar")
+        feature.search_student(busqueda,feature.date_student)
     #elif option == "4":
        
     elif option == "5":
@@ -22,6 +69,6 @@ while running:
      print("exit system ")
      break
     else:
-       print("Invalid option. Please try again.")
+       print("Invalid option. Please try again.")"""
 
             
